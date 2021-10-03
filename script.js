aa307c77b6b2ecda13a39d32652b682a4fddbd51
mersedee <mersede.rahbar@gmail.com>
2021-10-03 19:08:06 +0330

    add background grid

---
+++ script.js
@@ -0,0 +1,8 @@
+function multiplyNode(node, count, deep) {
+    for (var i = 0, copy; i < count - 1; i++) {
+        copy = node.cloneNode(deep);
+        node.parentNode.insertBefore(copy, node);
+    }
+}
+
+multiplyNode(document.querySelector('.grid-div'), 35, true);
