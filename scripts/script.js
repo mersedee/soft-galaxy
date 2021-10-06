a2e4dc5a2dc20100a0b0f2c5eaa04d9209e7918a
mersedee <mersede.rahbar@gmail.com>
2021-10-06 13:30:08 +0330

    edit grid background

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,25 @@
 const data = [
-    {number: '01', titlle: 'Wormhole',videoSrc: 'assets/videos/whormhole.mp4', desc: 'Wormholes were first theorized in 1916, though that wasn\'t what they were called at the time. While reviewing another physicist\'s solution to the equations in Albert Einstein\'s theory of general relativity, Austrian physicist Ludwig Flamm realized another solution was possible.'},
-    {number: '02', titlle: 'Eye of God', videoSrc: 'assets/videos/helix.mp4', desc: 'The Helix Nebula, also known as NGC 7293, is a planetary nebula (PN) located in the constellation Aquarius. Discovered by Karl Ludwig Harding probably before 1824, this object is one of the closest to the Earth of all the bright planetary nebulae'},
-    {number: '03', titlle: 'Blackhole',videoSrc: 'assets/videos/blackhole.mp4', desc: 'A black hole is a region of spacetime where gravity is so strong that nothing—no particles or even electromagnetic radiation such as light—can escape from it. The theory of general relativity predicts that a sufficiently compact mass can deform spacetime to form a black hole.'},
+    {
+        number: '01',
+        titlle: 'Wormhole',
+        videoSrc: 'assets/videos/whormhole.mp4',
+        desc: 'Wormholes were first theorized in 1916, though that wasn\'t what they were called at the time. While reviewing another physicist\'s solution to the equations in Albert Einstein\'s theory of general relativity, Austrian physicist Ludwig Flamm realized another solution was possible.'
+    },
+    {
+        number: '02',
+        titlle: 'Eye of God',
+        videoSrc: 'assets/videos/helix.mp4',
+        desc: 'The Helix Nebula, also known as NGC 7293, is a planetary nebula (PN) located in the constellation Aquarius. Discovered by Karl Ludwig Harding probably before 1824, this object is one of the closest to the Earth of all the bright planetary nebulae'
+    },
+    {
+        number: '03',
+        titlle: 'Blackhole',
+        videoSrc: 'assets/videos/blackhole.mp4',
+        desc: 'A black hole is a region of spacetime where gravity is so strong that nothing—no particles or even electromagnetic radiation such as light—can escape from it. The theory of general relativity predicts that a sufficiently compact mass can deform spacetime to form a black hole.'
+    },
 ]
 
-// create nodes
-function multiplyNode(node, count, deep) {
-    for (var i = 0, copy; i < count - 1; i++) {
-        copy = node.cloneNode(deep);
-        node.parentNode.insertBefore(copy, node);
-    }
-}
-
-multiplyNode(document.querySelector('.grid-div'), 35, true);
-
-
 // Find active index
-
 function setActiveData() {
     var child = document.getElementsByClassName("card-active");
     var index = Array.prototype.indexOf.call(child[0].parentNode.children, child[0]);
